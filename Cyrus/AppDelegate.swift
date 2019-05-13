73f30121dc96f10dedf5a1ad0e051082d654b43d
Baylus Tunnicliff <irishtunn@gmail.com>
2019-05-13 13:38:01 -0700

    Final

--- Cyrus/AppDelegate.swift
+++ Cyrus/AppDelegate.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2017 Josue Espinosa. All rights reserved.
 //
 
+//    Keywords to different schemes
+//    matching the best
+//    Chapter 15, section 15.3
+// inference queries, (give answer to question without a response) "I want a chinese resteraunt in moscow" "There is one in lewiston,
+//    table saying "indian food is similar to chionese", or whatever
+//    aggregate queries: Things like average, sum,
+
 import UIKit
 
 @UIApplicationMain
